7fcb9273a53e9ee70671d52deb34c73b28daf94c
kaydee98 <kay_dee98@hotmail.com>
2018-04-04 12:55:03 -0700

    project initial commit

---
+++ app/src/main/java/com/twistedgeinc/apps/tiwapa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.twistedgeinc.apps.tiwapa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
