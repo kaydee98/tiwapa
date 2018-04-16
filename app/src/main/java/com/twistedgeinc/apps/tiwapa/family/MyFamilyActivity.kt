1a0f821b23687f9bf201fc737ec321d2967a3271
kaydee98 <kay_dee98@hotmail.com>
2018-04-16 13:16:59 -0700

    Match floating bar for each tab

--- app/src/main/java/com/twistedgeinc/apps/tiwapa/family/MyFamilyActivity.kt
+++ app/src/main/java/com/twistedgeinc/apps/tiwapa/family/MyFamilyActivity.kt
@@ -2,11 +2,13 @@ package com.twistedgeinc.apps.tiwapa.family
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
+import android.view.View
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
 import com.twistedgeinc.apps.tiwapa.R
 import com.twistedgeinc.apps.tiwapa.utils.BottomNavigationViewHelper
@@ -14,6 +16,8 @@ import com.twistedgeinc.apps.tiwapa.utils.BottomNavigationViewHelper
 const val MENU_ITEM_NUMBER = 1
 
 class MyFamily : AppCompatActivity() {
+    private var addPersonFAB: FloatingActionButton? = null
+    private var addEventFAB: FloatingActionButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +25,24 @@ class MyFamily : AppCompatActivity() {
 
         setUpBottomNavigationView()
         setupViewPager()
+
+        addPersonFAB = findViewById<FloatingActionButton>(R.id.add_family_fab).apply {
+            setOnClickListener { addFamilyMember() }
+        }
+
+        addEventFAB = findViewById<FloatingActionButton>(R.id.add_event_fab).apply {
+            setOnClickListener { addFamilyEvent() }
+        }
+
     }
 
+    private fun addFamilyMember() {
+
+    }
+
+    private fun addFamilyEvent() {
+
+    }
     private fun setUpBottomNavigationView() {
         val bottomNavigationViewEx = findViewById(R.id.bottom_navigation) as BottomNavigationViewEx
         val bottomNavigationViewHelper = BottomNavigationViewHelper()
@@ -38,16 +58,42 @@ class MyFamily : AppCompatActivity() {
     private fun setupViewPager() {
 
         var viewPageAdapter = ViewPageAdapter(supportFragmentManager)
-        viewPageAdapter.addFragement(ParentFragment(), "Parents")
-        viewPageAdapter.addFragement(SiblingFragment(), "Siblings")
-        viewPageAdapter.addFragement(ChildrenFragment(), "Children")
-        viewPageAdapter.addFragement(LifeEventsFragment(), "Life Events")
+        viewPageAdapter.addFragment(ParentFragment(), getString(R.string.frag_parent_title))
+        viewPageAdapter.addFragment(SiblingFragment(), getString(R.string.frag_sibling_title))
+        viewPageAdapter.addFragment(ChildrenFragment(), getString(R.string.frag_children_title))
+        viewPageAdapter.addFragment(LifeEventsFragment(), getString(R.string.frag_life_events_title))
 
         var viewPager = findViewById<ViewPager>(R.id.fragment_view_pager)
         viewPager.adapter = viewPageAdapter
 
-        var tabsLayout = findViewById<TabLayout>(R.id.familty_tabs)
-        tabsLayout.setupWithViewPager(viewPager)
+        var tabsLayout = findViewById<TabLayout>(R.id.familty_tabs).apply {
+            setupWithViewPager(viewPager)
+
+            addOnTabSelectedListener( object: TabLayout.ViewPagerOnTabSelectedListener( viewPager) {
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                    super.onTabReselected(tab)
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                    super.onTabUnselected(tab)
+                    if (tab?.text.toString() == getString(R.string.frag_life_events_title)) {
+                        addEventFAB!!.visibility = View.GONE
+                    } else {
+                        addPersonFAB!!.visibility = View.GONE
+                    }
+
+                }
+
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    super.onTabSelected(tab)
+                   if (tab?.text.toString() == getString(R.string.frag_life_events_title)) {
+                       addEventFAB!!.visibility = View.VISIBLE
+                   } else {
+                       addPersonFAB!!.visibility = View.VISIBLE
+                   }
+                }
+            })
+        }
 
     }
 
@@ -55,7 +101,7 @@ class MyFamily : AppCompatActivity() {
         val fragmentList = ArrayList<Fragment>()
         val fragmentTitlesList = ArrayList<String>()
 
-        fun addFragement(fragment: Fragment, title: String): Unit {
+        fun addFragment(fragment: Fragment, title: String): Unit {
             fragmentList.add(fragment)
             fragmentTitlesList.add(title)
         }
