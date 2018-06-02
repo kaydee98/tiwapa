3f2386afe2c9e6cf58b47d073246e4ea24212875
kaydee98 <kay_dee98@hotmail.com>
2018-06-02 12:13:28 -0700

    update Instrument Test

--- app/src/androidTest/java/com/twistedgeinc/apps/tiwapa/MyFamilyInstrumentedTest.kt
+++ app/src/androidTest/java/com/twistedgeinc/apps/tiwapa/MyFamilyInstrumentedTest.kt
@@ -1,22 +1,26 @@
 package com.twistedgeinc.apps.tiwapa
 
 import android.support.test.filters.LargeTest
-import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import com.twistedgeinc.apps.tiwapa.family.MyFamilyActivity
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import com.twistedgeinc.apps.tiwapa.family.MyFamilyActivity
 import org.hamcrest.Matchers.allOf
+import org.junit.Rule
 
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class MyFamilyInstrumentedTest {
+
+    @get:Rule
+    val myFamilyActivityBehaviourRule = ActivityTestRule(MyFamilyActivity::class.java)
+
 
     @Test
     fun shouldDisplaySiblingsWhenInSiblingTab() {
